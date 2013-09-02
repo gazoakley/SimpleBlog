764e4aaa08ec2e428f2430294f498a28b1ac7e60
Gareth Oakley <gaz at the2ndproject dot com>
2013-09-02 19:08:57 +0100

    Basic PostModule methods

--- SimpleBlog/PostModule.cs
+++ SimpleBlog/PostModule.cs
@@ -10,10 +10,48 @@ namespace SimpleBlog
     {
         public PostModule()
         {
-            Get["/"] = _ =>
-            {
-                return "Hello world";
-            };
+            Get["/"] = _ => Index();
+            Get["/create"] = _ => Create();
+            Post["/"] = _ => Store();
+            Get["/{id}"] = _ => Show(_.id);
+            Get["/{id}/edit"] = _ => Edit(_.id);
+            Put["/{id}"] = _ => Update(_.id);
+            Delete["/{id}"] = _ => Destroy(_.id);
+        }
+
+        public string Index()
+        {
+            return "index";
+        }
+
+        public string Create()
+        {
+            return "create";
+        }
+
+        public string Store()
+        {
+            return "store";
+        }
+
+        public string Show(int id)
+        {
+            return "show";
+        }
+
+        public string Edit(int id)
+        {
+            return "edit";
+        }
+
+        public string Update(int id)
+        {
+            return "update";
+        }
+
+        public string Destroy(int id)
+        {
+            return "destroy";
         }
     }
 }
