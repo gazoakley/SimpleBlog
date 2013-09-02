e5415d64e5ef11441b22c831f4ce6a2ef55cf2f9
Gareth Oakley <gaz at the2ndproject dot com>
2013-09-02 19:00:56 +0100

    Basic NancyModule/hosting

---
+++ SimpleBlog/PostModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+
+namespace SimpleBlog
+{
+    public class PostModule : NancyModule
+    {
+        public PostModule()
+        {
+            Get["/"] = _ =>
+            {
+                return "Hello world";
+            };
+        }
+    }
+}
--- SimpleBlog/Program.cs
+++ SimpleBlog/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Nancy.Hosting.Self;
 
 namespace SimpleBlog
 {
@@ -9,6 +10,10 @@ namespace SimpleBlog
     {
         static void Main(string[] args)
         {
+            var nancyHost = new NancyHost(new Uri("http://localhost:1234/"));
+            nancyHost.Start();
+            Console.Read();
+            nancyHost.Stop();
         }
     }
 }
