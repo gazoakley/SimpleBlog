52e8342701c0581883e4b0c87fff55629119e8e7
Gareth Oakley <gaz at the2ndproject dot com>
2013-09-02 21:57:42 +0100

    Add Post model

---
+++ SimpleBlog/Entities/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleBlog.Entities
+{
+    public class Post
+    {
+        public virtual int Id { get; protected set; }
+        public virtual string Subject { get; set; }
+        public virtual string Body { get; set; }
+    }
+}
