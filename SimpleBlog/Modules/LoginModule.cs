edceb4bef81a33a4b416b7b31b73f73b1b08f010
Gareth Oakley <gaz at the2ndproject dot com>
2013-09-08 12:50:54 +0100

    Initial auth module

---
+++ SimpleBlog/Modules/LoginModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+using Nancy.Authentication.Forms;
+
+namespace SimpleBlog.Modules
+{
+    public class LoginModule : NancyModule
+    {
+        public LoginModule()
+        {
+            Get["/login"] = parameters =>
+            {
+                // Called when the user visits the login page or is redirected here because
+                // an attempt was made to access a restricted resource. It should return
+                // the view that contains the login form
+                return View["auth/login"];
+            };
+
+            Get["/logout"] = parameters =>
+            {
+                // Called when the user clicks the sign out button in the application. Should
+                // perform one of the Logout actions (see below)
+                return this.LogoutAndRedirect("/");
+            };
+
+            Post["/login"] = parameters =>
+            {
+                // Called when the user submits the contents of the login form. Should
+                // validate the user based on the posted form data, and perform one of the
+                // Login actions (see below)
+                return this.Login(Guid.NewGuid());
+            };
+        }
+    }
+}
