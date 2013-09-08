38bb799e97637a3f993623e96550f625388f2d06
Gareth Oakley <gaz at the2ndproject dot com>
2013-09-08 12:50:20 +0100

    Create User entity class

---
+++ SimpleBlog/Entities/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+using NHibernate;
+using Nancy;
+using Nancy.Authentication.Forms;
+using Nancy.Security;
+
+namespace SimpleBlog.Entities
+{
+    public class User : IUserIdentity
+    {
+        public virtual Guid Id { get; protected set; }
+        public virtual string UserName { get; set; }
+        public virtual string Password { get; set; }
+        public virtual IEnumerable<string> Claims { get; set; }
+    }
+
+    public class UserMappingOverride : IAutoMappingOverride<User>
+    {
+        public void Override(AutoMapping<User> mapping)
+        {
+            mapping.IgnoreProperty(p => p.Claims);
+        }
+    }
+
+    public class UserMapper : IUserMapper
+    {
+        private ISessionFactory _sessionFactory;
+
+        public UserMapper(ISessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory;
+        }
+
+        public IUserIdentity GetUserFromIdentifier(Guid identifier, NancyContext context)
+        {
+            using (var session = _sessionFactory.OpenSession())
+            {
+                var user = session.Get<User>(identifier);
+                return user;
+            }
+        }
+    }
+}
